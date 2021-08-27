5170123c5887b9413f5da30cfb1f2befe0ada5ad
eugarcia <eugarcia@bancolombia.com.co>
2021-08-27 16:14:19 -0500

    added events

--- extra-webpack.config.js
+++ extra-webpack.config.js
@@ -3,6 +3,15 @@ const singleSpaAngularWebpack = require('single-spa-angular/lib/webpack').defaul
 module.exports = (config, options) => {
   const singleSpaWebpackConfig = singleSpaAngularWebpack(config, options);
 
+  let removeDup = {};
+  singleSpaWebpackConfig.entry.main = singleSpaWebpackConfig.entry.main.filter((item) => {
+  if(!removeDup[item]) {
+    removeDup[item] = true;
+    return true;
+  }
+  return false;
+  });
+
   // Feel free to modify this webpack config however you'd like to
   return singleSpaWebpackConfig;
 };
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { singleSpaPropsSubject } from 'src/single-spa/single-spa-props';
+import { login } from '../../../microfront-demo-container/src/';
+
 
 @Component({
   selector: 'mf-login',
@@ -9,10 +13,44 @@ export class AppComponent implements OnInit{
   title = 'mf-login';
   email: String = '';
   psw: String = '';
-  ngOnInit(): void {}
-  
 
+  singleSpaProps?: any;
+  subscription?: Subscription;
+
+  constructor(private ChangeDetectorRef:ChangeDetectorRef){
+
+  }
+
+  ngOnInit(): void {
+    this.initSubscription();
+  }
+
+  initSubscription(){
+    this.subscription = singleSpaPropsSubject.subscribe(
+      props => {
+        this.singleSpaProps = props;
+        this.lookForEvents();
+      }
+    );
+  }
+
+  lookForEvents(){
+    this.singleSpaProps['EventBus'].on('onUserLogged', this.onUserLogged.bind(this));
+  }
+
+  onUserLogged(data: any){
+    console.log("user logged listening login ", data);
+  }
+  
   ingresar() {
     console.log(`Ingresar Email: ${this.email} - psw: ${this.psw}`);
+
+    login({email: this.email, pass: this.psw}).subscribe(data => {
+      this.singleSpaProps['EventBus'].emit({name:'onUserLogged',data});
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription?.unsubscribe();
   }
 }
